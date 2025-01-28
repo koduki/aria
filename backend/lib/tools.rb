169f37fc2c5ab6dd5d0a3e69a1092220513feb34
koduki <koduki@nklab.dev>
2025-01-29 04:01:09 +0900

    Add function_call

--- backend/lib/tools.rb
+++ backend/lib/tools.rb
@@ -2,13 +2,11 @@ require_relative './decorator'
 
 module Tools
   Gemini::FunctionDecorator.doc(:find_movies,
-  "find movie titles currently playing in theaters based on any description, genre, title words, etc.",{
-  location: ["string","The city and state, e.g. San Francisco, CA or a zip code e.g. 95616"],
-  description: ["string","required","Any kind of description including category or genre, title words, attributes, etc."]
-})
+    "find movie titles currently playing in theaters based on any description, genre, title words, etc.",{
+    location: ["string","The city and state, e.g. San Francisco, CA or a zip code e.g. 95616"],
+    description: ["string","required","Any kind of description including category or genre, title words, attributes, etc."]
+  })
   def self.find_movies(location, description)
-
-
     puts "find_movies called with location: #{location}, description: #{description}"
     return "Hello"
   end
