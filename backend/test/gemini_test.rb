34ae242ad1920e665c5ae5d6ffb713c7c0eab187
koduki <koduki@nklab.dev>
2025-01-29 01:28:04 +0900

    Add test

--- backend/test/gemini_test.rb
+++ backend/test/gemini_test.rb
@@ -2,10 +2,7 @@ require 'minitest/autorun'
 require_relative '../lib/gemini'
 
 class GeminiTest < Minitest::Test
-  #
-  # LLMのテストなので実行結果のAssertは意図的にせずに出力をチェックのみ
-  #
-  def test_chat
+  def _init_gemini
     api_key = File.read(File.join(Dir.home, '.secret', 'gemini.txt')).strip
     gemini = Gemini::Gemini.new(
       credentials: {
@@ -14,6 +11,13 @@ class GeminiTest < Minitest::Test
       },
       options: { model: 'gemini-2.0-flash-exp', system_instruction:"これは対話アプリなので回答は短くしてください。" }
     )
+  end
+
+  #
+  # LLMのテストなので実行結果のAssertは意図的にせずに出力をチェックのみ
+  #
+  def test_chat
+    gemini = _init_gemini()
 
     history = Gemini::History.new
     r = gemini.chat("今日の映画は？", history)
@@ -22,5 +26,22 @@ class GeminiTest < Minitest::Test
     puts "response"
     p r[:response]
   end
-end
 
+  def test_generate_content_with_function_calling
+    gemini = _init_gemini()
+
+    payload = {
+      "contents": [{
+        "parts":[
+          {"text": "List 5 popular cookie recipes"}
+          ]
+      }]
+    }
+
+    r = gemini.generate_content(payload)
+    puts "request"
+    p r[:request]
+    puts "response(function_calling)"
+    p r[:response]
+  end
+end
