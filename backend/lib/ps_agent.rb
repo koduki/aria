ace63c55c5d0d27aacab806741a47211f1663b60
koduki <koduki@nklab.dev>
2025-02-16 18:09:22 +0900

    Add to support PowerShell gen

--- backend/lib/ps_agent.rb
+++ backend/lib/ps_agent.rb
@@ -1,15 +1,11 @@
 load 'lib/powershell.rb'
-ps = PowerShell.new
-
-
-
-r = ps.invoke("echo Hello")
-p r
+@ps = PowerShell.new
 
 require 'json'
 
 require_relative './gemini/gemini'
 
+history = Gemini::History.new
 @api_key = File.read(File.join(Dir.home, '.secret', 'gemini.txt')).strip
 sysprompt = open('prompts/powershell.txt').read
 client = Gemini::Gemini.new(
@@ -20,53 +16,39 @@ credentials: {
 options: { model: 'gemini-2.0-flash-exp', system_instruction:sysprompt, json_mode:true}
 )
 
-contents='Videoディレクトリの7月の動画をフォルダにまとめて'
-r = client.generate_content({
-    contents: [{ role: 'user', parts: [{ text: contents }] }]
-})
-
-# puts r[:response]["content"]["parts"][0]["text"]
 
 def parse(text)
     trimed_data = text.sub(/\A```json\s*/, '').sub(/\s*```\z/, '')
     JSON.parse(trimed_data)
 end
 
-# p r
+def exec command
+    command_cp932 = command.encode("CP932", invalid: :replace, undef: :replace)
+    @ps.invoke(command_cp932)
+end
 
+def generate_content exec_r
+    contents = if exec_r[:status] == 0
+        exec_r[:stdout]
+        contents="以下の実行結果になりました。ユーザリクエストを満たす次のコマンドを生成してください.\n#{exec_r[:stderr]}"
+    else
+        contents="以下のエラーが発生しました。問題解決のためにコマンドを修正してください.\n#{exec_r[:stderr]}"
+    end
+    contents
+end
 
-response = parse(r[:response]["content"]["parts"][0]["text"])
+content = "Videoディレクトリの8月の動画をフォルダにまとめて"
+while(true) do
+    llm_res = client.chat(content, {history:history})
+    response = parse(llm_res[:response]["content"]["parts"][0]["text"])
+    p response["finish"]
+    break if response["finish"]
+    puts "thinking: #{response["thinking"]}"
 
-# puts response["thinking"]
-task = response["task"]
-# p task
-# r = ps.invoke(task)
-# p r
+    command = response["task"]
+    puts "cmd: #{command}"
+    exec_r = exec command
+    content = generate_content(exec_r)
+end
 
-
-# テストコード例
-# result = ps.invoke("$videoPath = [Environment]::GetFolderPath('MyVideos'); " \
-#                 "$source = Join-Path $videoPath '2024年7月'; " \
-#                 "if (!(Test-Path -Path $source)) { Write-Host 'Source directory does not exist.' }; " \
-#                 "Get-ChildItem -Path $source")
-# puts "Result: #{result[:stdout]}"
-
-multiline_command = <<~POWERSHELL
-$x = 10
-$y = 20
-Write-Host ($x + $y)
-POWERSHELL
-p multiline_command
-
-command = <<~POWERSHELL
-$videoPath = [Environment]::GetFolderPath('MyVideos')
-$targetFolderName = '7月動画'
-$targetPath = Join-Path -Path $videoPath -ChildPath $targetFolderName
-New-Item -ItemType Directory -Path $targetPath
-Get-ChildItem -Path $targetPath 
-POWERSHELL
-command_cp932 = command.encode("CP932", invalid: :replace, undef: :replace)
-
-
-# p ps.invoke('$videoPath = [Environment]::GetFolderPath(\'MyVideos\')\n$targetFolderName = \'7月動画\'\n$targetPath = Join-Path -Path $videoPath -ChildPath $targetFolderName\nif (!(Test-Path -Path $targetPath)) {\n  New-Item -ItemType Directory -Path $targetPath\n}\nGet-ChildItem -Path $videoPath -File | Where-Object {$_.LastWriteTime.Month -eq 7} | ForEach-Object {Move-Item -Path $_.FullName -Destination $targetPath}')
-p ps.invoke(command_cp932)
+puts "DONE"
