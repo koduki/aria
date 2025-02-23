b023aba615aceb0fb308efaa745d08f48f549b42
koduki <koduki@nklab.dev>
2025-02-23 15:59:04 +0900

    Add aria class

--- backend/app.rb
+++ backend/app.rb
@@ -1,29 +1,13 @@
 require 'sinatra'
 require 'json'
-require_relative './lib/gemini/gemini'
-require_relative './lib/tools'
+require_relative './lib/aria'
 
-# API keyの読み込み
-api_key = File.read(File.join(Dir.home, '.secret', 'gemini.txt')).strip
-
-# クライアントオブジェクトを作成
-client = Gemini::Gemini.new(
-  credentials: {
-    service: 'generative-language-api',
-    api_key: api_key
-  },
-  options: { model: 'gemini-2.0-flash-exp', server_sent_events: true, system_instruction:"あなたはコンピュータサイエンスの女子大生です。かわいいです。" }
-)
-
-# グローバル変数として History クラスのインスタンスを保持
-$history = Gemini::History.new
+set :aria, Aria.new
 
 post '/api/chat' do
   content_type :json
   request_body = JSON.parse(request.body.read)
   text = request_body['text']
 
-  result = client.chat(text, {history:$history, tools:Tools})
-
-  result.to_json
+  settings.aria.chat(text)
 end
---
+++ backend/lib/aria.rb
@@ -0,0 +1,27 @@
+require_relative './gemini/gemini'
+require_relative './tools'
+
+class Aria
+  def initialize
+    # API keyの読み込み
+    api_key = File.read(File.join(Dir.home, '.secret', 'gemini.txt')).strip
+
+    # クライアントオブジェクトを作成
+    @client = Gemini::Gemini.new(
+      credentials: {
+        service: 'generative-language-api',
+        api_key: api_key
+      },
+      options: { model: 'gemini-2.0-flash-exp', server_sent_events: true, system_instruction:"あなたはコンピュータサイエンスの女子大生です。かわいいです。" }
+    )
+
+    # グローバル変数として History クラスのインスタンスを保持
+    @history = Gemini::History.new
+  end
+
+  def chat text
+    result = @client.chat(text, {history:@history, tools:Tools})
+
+    result.to_json
+  end
+end
